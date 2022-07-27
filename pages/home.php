aea1c56e23965fa5787d1b7d13787c34e98b9c60
DanielMikitiuk <86248084+DanielMikitiuk@users.noreply.github.com>
2022-07-27 12:54:44 +0200

    registration-form-php-dz3
    )

---
+++ pages/home.php
@@ -0,0 +1,15 @@
+<h1>Добавить страницы:</h1>
+<br>Регистрация (все поля обязательные)
+<ul>
+
+    <li>login</li>
+    <li>email</li>
+    <li>password</li>
+
+</ul>
+<br> Вход (все поля обязательные)
+<ul>
+    <li>email</li>
+    <li>password</li>
+
+</ul>
